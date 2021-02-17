c4578d9697bab06feb253aace01fe3230556b11d
Akhil Lawrence <akhilputhiry@gmail.com>
2021-02-17 22:12:34 +0530

    bump up python runtime

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
 import os
+from base64 import b64encode
+from urllib.parse import urlencode
+
 import requests
 from flask import Flask
 from flask_cors import CORS
-from base64 import b64encode
-from urllib.parse import urlencode
 
 
 app = Flask(__name__)
@@ -13,11 +14,13 @@ CORS(app)
 
 @app.route("/ping")
 def ping():
+    """ health check """
     return "pong"
 
 
 @app.route("/docs/<pdf>")
 def doc(pdf):
+    """ api to retrieve pdf documents as base64 encoded strings """
     with open(f"./docs/{pdf}", "rb") as f:
         data = f.read()
         data = b64encode(data).decode("utf-8")
@@ -26,18 +29,21 @@ def doc(pdf):
 
 @app.route("/youtube/<channel>")
 def youtube(channel):
-    params = {
-        "channelId": channel,
-        "maxResults": 25,
-        "order": "date",
-        "part": "snippet",
+    """ api to retrieve latest videos of the channel specified """
+    params = urlencode({
         "key": os.environ["API_KEY"],
-    }
-    response = requests.get("https://youtube.googleapis.com/youtube/v3/search?" + urlencode(params))
+        "part": "snippet",
+        "order": "date",
+        "maxResults": 25,
+        "channelId": channel,
+    })
+    url = f"https://youtube.googleapis.com/youtube/v3/search?{params}"
+    response = requests.get(url)
     response.raise_for_status()
     return response.json()
 
 
 if __name__ == "__main__":
+    """ run the flask server """
     port = os.getenv("PORT", 5000)
     app.run(host="0.0.0.0", port=port)
